dc8fb471aeea58876233ae25df1689760fe2b406
Matvey Romanov <youtuberm@yandex.ru>
2023-10-01 19:37:30 +0300

    Добавлен аптайм

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import logging
 import asyncio
 from aiogram import Bot, Dispatcher, types
@@ -21,9 +20,9 @@ ssh = client.invoke_shell()
 def default_keyboard():
     builder = InlineKeyboardBuilder()
     builder.row(types.InlineKeyboardButton(text='🔴 Остановить процесс', callback_data='empty'))
-    builder.row(types.InlineKeyboardButton(text='🆙 Аптайм', callback_data='empty'))
-    builder.row(types.InlineKeyboardButton(text='🔄 Перезагрузить сервер', callback_data='empty'))
-    builder.row(types.InlineKeyboardButton(text='🗂️ Бэкапы', callback_data='backups'))
+    builder.row(types.InlineKeyboardButton(text='🆙 Аптайм', callback_data='uptime'))
+    builder.row(types.InlineKeyboardButton(text='🔄 Перезагрузить сервер', callback_data='reboot'))
+    builder.row(types.InlineKeyboardButton(text='🗂 Бэкапы', callback_data='backups'))
     builder.row(types.InlineKeyboardButton(text='🔑 Сбросить пароль администратора', callback_data='empty'))
     builder.adjust(2, 2)
     return builder.as_markup()
@@ -33,6 +32,14 @@ def default_keyboard():
 async def command_start_handler(message: Message) -> None:
     await message.answer(f'Этот бот позволяет контролировать корректность работы сервера.\n'
                          f'Для взаимодействия используй кнопки ниже.', reply_markup=default_keyboard())
+
+
+@dp.callback_query(F.data == 'reboot')
+async def backups(callback: types.CallbackQuery):
+    await callback.message.answer('Выполняется перезагрузка сервера...')
+    ssh.send('reboot now\n')
+    time.sleep(20)
+    await callback.message.answer('Сервер успешно перезагужен')
 
 
 @dp.callback_query(F.data == 'backups')
@@ -58,12 +65,9 @@ async def new_backup(callback: types.CallbackQuery):
     time.sleep(1)
     ssh.send('rm -r movies_website\n')
     time.sleep(1)
-    print(ssh.recv(3000))
     ssh.send('cd ~/backup\n')
     time.sleep(1)
-    print(ssh.recv(3000))
     ssh.send('npm install\n')
-    print(ssh.recv(3000))
     time.sleep(1)
     ssh.send('mv ~/backup ~/movies_website\n')
     time.sleep(7)
@@ -71,6 +75,19 @@ async def new_backup(callback: types.CallbackQuery):
     time.sleep(1)
     ssh.send('pm2 start app.js --name "movies_website" --watch && pm2 save\n')
     await callback.message.answer('Восстановлено из резервной копии\nПроцесс перезапущен')
+
+
+@dp.callback_query(F.data == 'uptime')
+async def uptime(callback: types.CallbackQuery):
+    stdin, stdout, stderr = client.exec_command('pm2 status')
+    pm2_status_output = stdout.read().decode('utf-8')
+    lines = pm2_status_output.strip().split('\n')
+    cleaned_output = [line for line in lines if not (line.startswith('┌') or line.startswith('├') or line.startswith('└'))]
+    header = cleaned_output[0].split('│')[1:-1]
+    data = cleaned_output[1].split('│')[1:-1]
+    key_value_dict = {h.strip(): d.strip() for h, d in zip(header, data)}
+    formatted_str = '\n'.join([f"{k}: {v}" for k, v in key_value_dict.items()])
+    await callback.message.answer(f'<pre>{formatted_str}</pre>')
 
 
 @dp.callback_query(F.data == 'go_back')
@@ -83,7 +100,6 @@ async def backups(callback: types.CallbackQuery):
 async def main() -> None:
     bot = Bot(token='6485242380:AAFlF7Ywcy6W4tl6nsQ1PDdNaVtRWVxLl9g', parse_mode=ParseMode.HTML)
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
